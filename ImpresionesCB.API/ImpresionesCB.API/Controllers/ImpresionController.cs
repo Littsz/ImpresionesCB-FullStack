f005b6da87798a14b9b109b713b54d862d7c6bce
Israel Rodriguez Rivera <israemoxito@gmail.com>
2026-03-31 10:26:54 -0600

    Solución de conexión API-Vue y configuración de CORS

---
+++ ImpresionesCB.API/ImpresionesCB.API/Controllers/ImpresionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ImpresionesCB.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImpresionController : ControllerBase
+    {
+        [HttpPost("imprimir")]
+        public IActionResult ImprimirEtiquetas([FromBody] List<string> listaCodigos)
+        {
+            if (listaCodigos == null || listaCodigos.Count == 0)
+            {
+                return BadRequest("La lista de códigos está vacía.");
+            }
+
+
+            foreach (var codigo in listaCodigos)
+            {
+                Console.WriteLine($"Preparando impresión para el código: {codigo}");
+            }
+
+            return Ok(new { mensaje = "¡Conexión exitosa! La API recibió los " + listaCodigos.Count + " códigos correctamente." });
+        }
+    }
+}
--- ImpresionesCB.API/ImpresionesCB.API/Program.cs
+++ ImpresionesCB.API/ImpresionesCB.API/Program.cs
@@ -1,31 +1,35 @@
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-// Configurar la conexión a SQL Server
+
 builder.Services.AddDbContext<ImpresionesCB.API.Data.AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
-builder.Services.AddCors(options => options.AddPolicy("Open", b => b.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+builder.Services.AddOpenApi();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirTodo", policy =>
+    {
+        policy.SetIsOriginAllowed(_ => true) 
+              .AllowAnyHeader()             
+              .AllowAnyMethod();            
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+
+
 
 app.UseHttpsRedirection();
-app.UseCors("Open");
+app.UseCors("PermitirTodo");
+
+
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
